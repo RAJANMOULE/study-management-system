9b5d49cd5a4b1ad172fb31289262b50dcbaf93fa
MOULERAJAN G <96585317+RAJANMOULE@users.noreply.github.com>
2022-11-18 21:24:32 +0530

    data type result python file

---
+++ 5.2.datatypes_result.py
@@ -0,0 +1,195 @@
+#! C:\Users\MOULERAJAN G\AppData\Local\Programs\Python\Python310\python.exe
+
+print("Content-type: text/html\n\r")
+
+import cgi
+
+
+f=cgi.FieldStorage()
+
+k=[]
+
+k1=f.getvalue('a1')
+k2=f.getvalue('a2')
+k3=f.getvalue('a3')
+k4=f.getvalue('a4')
+k5=f.getvalue('a5')
+k6=f.getvalue('a6')
+k7=f.getvalue('a7')
+k8=f.getvalue('a8')
+k9=f.getvalue('a9')
+k10=f.getvalue('a10')
+k11=f.getvalue('a11')
+k12=f.getvalue('a12')
+k13=f.getvalue('a13')
+k14=f.getvalue('a14')
+k15=f.getvalue('a15')
+k16=f.getvalue('a16')
+k17=f.getvalue('a17')
+k18=f.getvalue('a18')
+k19=f.getvalue('a19')
+k20=f.getvalue('a20')
+k21=f.getvalue('a21')
+k22=f.getvalue('a22')
+
+
+if k1=='3':
+    k.append(1)
+
+if k2=='3':
+    k.append(1)
+
+if k3=='3':
+    k.append(1)
+
+if k4=='1':
+    k.append(1)
+
+if k5=='3':
+    k.append(1)
+
+if k6=='2':
+    k.append(1)
+
+if k7=='4':
+    k.append(1)
+
+if k8=='1':
+    k.append(1)
+
+if k9=='2':
+    k.append(1)
+
+if k10=='1':
+    k.append(1)
+
+if k11=='1':
+    k.append(1)
+
+if k12=='1':
+    k.append(1)
+
+if k13=='3':
+    k.append(1)
+
+if k14=='3':
+    k.append(1)
+
+if k15=='1':
+    k.append(1)
+
+if k16=='4':
+    k.append(1)
+
+if k17=='2':
+    k.append(1)
+
+if k18=='3':
+    k.append(1)
+
+if k19=='3':
+    k.append(1)
+
+if k20=='4':
+    k.append(1)
+
+l=sum(k)
+
+x=[]
+y=[]
+from mysql.connector import connect
+
+db=connect(
+        host='localhost',
+        user='root',
+        password='Gmoule@123',
+        database='ssms'
+    )
+mycursor=db.cursor()
+
+sql=("SELECT time,concept FROM ssms.time")
+
+mycursor.execute(sql)
+data=mycursor.fetchall()
+
+for i,j in data:
+    x.append(i)
+    y.append(j)
+    
+total_time=str(x[-1])
+concept=str(y[-1])
+import smtplib
+
+# creates SMTP session
+s = smtplib.SMTP('smtp.gmail.com', 587)
+
+# start TLS for security
+s.starttls()
+
+# Authentication
+s.login("gmoulerajan@gmail.com", "yzddqrsuotakchow")
+
+# message to be sent
+message = f'''Hi. we are from Abc learning academy. Your son studying python developer course in our academy. Well, your son takes {total_time} seconds to study the {concept} concept. Then he takes {k22} seconds to finish quiz test and he got {l} marks out of 20. thank you '''
+
+try:
+# sending the mail
+    s.sendmail("gmoulerajan@gmail.com", "janarithish@gmail.com", message)
+
+# terminating the session
+    
+#s.quit()
+except:
+    print('not ok')
+ 
+
+print('''
+<html>
+<head>
+<style>
+.center {
+  background-image: linear-gradient(-225deg,#E3FDF5 50%, #FFE6FA 40%);  position: relative;
+  top:120px;
+  z-index: 1;
+  max-width: 360px;
+  margin: 0 auto 100px;
+  padding: 45px;
+  text-align: center;
+  color:blue;
+  box-shadow: 0 0 20px 0 rgba(55, 55, 55, 0.2), 0 5px 5px 0 rgba(55, 55, 55, 0.24);
+  }
+body {
+    background-image: linear-gradient(-225deg, #E3FDF5 0%, #FFE6FA 100%);
+background-image: linear-gradient(to top, #e757b0 0%, #4758b8 100%);
+background-attachment: fixed;
+  background-repeat: no-repeat;
+
+    font-family: 'Vibur', cursive;
+/*   the main font */
+    font-family: 'Abel', sans-serif;
+opacity: .95;
+/* background-image: linear-gradient(to top, #d9afd9 0%, #97d9e1 100%); */
+}
+</style>
+</head>
+<body>
+<div class='center' style='background-color:pink'>
+<h2><center>
+Quiz concept :      Datatypes <br><br>
+
+Total Question :  20 <br><br>
+''')
+print(f'''
+Correct Answer :  {l} <br><br>
+
+Score :           {l}/20
+
+<br>
+<br>
+<br>
+<a href='1.signup.html'>Signup</a> &emsp;&emsp;&emsp; <a href='7.logout.html'>Logout</a>
+</center>
+</h2>
+</div>
+</body>
+</html>''')
